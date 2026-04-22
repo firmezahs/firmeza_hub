5666b36f8661250e86c018f1cda61c4ac04b5535
Khushi <khushi.firmeza@gmail.com>
2026-04-22 17:35:08 +0530

    fixed upload in documents

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.get("/dashboard", (req, res) => {
 // =======================
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
